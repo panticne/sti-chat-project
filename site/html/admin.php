48a128bbee88ae62c4fe73590bf97b62c977f6f6
nyg <nyg@users.noreply.github.com>
2019-10-07 17:13:14 +0200

    Advance with admin implementation.

--- site/html/admin.php
+++ site/html/admin.php
@@ -16,8 +16,6 @@ include 'include/html_menu.php';
 
 if (isset($_POST['delete'])) {
 
-    var_dump($_POST);
-
     try {
         $db = init_db();
         $stmt = $db->prepare('DELETE FROM user WHERE id = :id');
@@ -29,7 +27,48 @@ if (isset($_POST['delete'])) {
             echo '<p>Utilisateur supprimé !</p>';
         }
         else {
-            echo '<p>Échec de la suppression de l\'utilisateur !</p>';
+            echo '<p>Erreur lors de la suppression de l\'utilisateur !</p>';
+        }
+    }
+    catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+elseif (isset($_POST['save'])) {
+
+    try {
+        $db = init_db();
+
+        // check if user exists
+        $stmt = $db->prepare('SELECT COUNT(*) FROM user WHERE id = :id');
+        $stmt->execute(['id' => $_POST['id']]);
+        $res = $stmt->fetch();
+
+        if ($res[0] == 1) {
+            // update user
+            $stmt = $db->prepare('UPDATE user SET firstname = :firstname, lastname = :lastname, password = :password, admin = :admin, active = :active WHERE id = :id');
+            $stmt->execute(['firstname' => $_POST['firstname'], 'lastname' => $_POST['lastname'], 'password' => $_POST['password'], 'admin' => $_POST['admin'] == "on", 'active' => $_POST['active'] == "on", 'id' => $_POST['id']]);
+            $res = $stmt->rowCount();
+
+            if ($res == 1) {
+                echo '<p>Utilisateur mis à jour !</p>';
+            }
+            else {
+                echo '<p>Erreur lors de la mise à jour de l\'utilisateur !</p>';
+            }
+        }
+        else {
+            // insert user
+            $stmt = $db->prepare('INSERT INTO user (firstname, lastname, username, password, admin, active) VALUES (:firstname, :lastname, :username, :password, :admin, :active)');
+            $stmt->execute(['firstname' => $_POST['firstname'], 'lastname' => $_POST['lastname'], 'username' => $_POST['username'], 'password' => $_POST['password'], 'admin' => $_POST['admin'] == "on", 'active' => $_POST['active'] == "on"]);
+            $res = $stmt->rowCount();
+
+            if ($res == 1) {
+                echo '<p>Utilisateur créé !</p>';
+            }
+            else {
+                echo '<p>Erreur lors de la création de l\'utilisateur !</p>';
+            }
         }
     }
     catch (PDOException $e) {
@@ -57,6 +96,7 @@ else {
         echo '<option value="' . $user['id'] . '">' . $user['username'] . '</option>';
     }
 
+    unset($user);
     echo '</select>';
 
     echo '<button type="submit" name="delete">Supprimer</button>';
@@ -64,65 +104,44 @@ else {
 
     echo '</form>';
 
-    echo '<form action="admin.php" method="post">';
-    echo '<select name="id">';
-    echo '<option>Choisissez un utilisateur</option>';
-
-    try {
-        $db = init_db();
-
-        // retrieve all user
-        $stmt = $db->query('SELECT * FROM user');
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $db = null;
+    if (isset($_POST['edit'])) {
+        try {
+            $db = init_db();
+            $stmt = $db->prepare('SELECT * FROM user WHERE id = :id');
+            $stmt->execute(['id' => $_POST['id']]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
-    catch (PDOException $e) {
-        echo $e->getMessage();
-    }
+    ?>
 
-    foreach ($users as $user) {
-        echo '<option value="' . $user['id'] . '">' . $user['username'] . '</option>';
-    }
+    <form action="admin.php" method="post">
+        <input type="hidden" name="id" value="<?= $user['id'] ?>">
 
-    echo '</select>';
+        <label for="username">Utilisateur</label>
+        <input name="username" id="username" value="<?= $user['username'] ?>">
 
-    echo '<button type="submit" name="delete">Supprimer</button>';
-    echo '<button type="submit" name="edit">Modifier</button>';
+        <label for="firstname">Prénom</label>
+        <input name="firstname" id="firstname" value="<?= $user['firstname'] ?>">
 
-    echo '</form>';
+        <label for="lastname">Nom</label>
+        <input name="lastname" id="lastname" value="<?= $user['lastname'] ?>">
 
-    echo '<form action="admin.php" method="post">';
-    echo '<input name="id">';
-    echo '<option>Choisissez un utilisateur</option>';
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" value="<?= $user['password'] ?>">
 
-    try {
-        $db = init_db();
+        <label for="admin">Admin</label>
+        <input name="admin" id="admin" type="checkbox" <?= $user['admin'] ? "checked" : "" ?>>
 
-        // retrieve all user
-        $stmt = $db->query('SELECT * FROM user');
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $db = null;
-    }
-    catch (PDOException $e) {
-        echo $e->getMessage();
-    }
+        <label for="active">Actif</label>
+        <input name="active" id="active" type="checkbox" <?= $user['active'] ? "checked" : "" ?>>
 
-    foreach ($users as $user) {
-        echo '<option value="' . $user['id'] . '">' . $user['username'] . '</option>';
-    }
+        <button type="submit" name="save">Sauver</button>
+    </form>
 
-    echo '</select>';
-
-    echo '<button type="submit" name="delete">Supprimer</button>';
-    echo '<button type="submit" name="edit">Modifier</button>';
-
-    echo '</form>';
-
+    <?php
 }
-?>
-
-    <!-- TODO  -->
-
-<?php
 
 include 'include/html_footer.php';
