5c522db9a25f860dd3a6b5c8954139d329f168e5
nyg <nyg@users.noreply.github.com>
2019-10-16 16:06:34 +0200

    Change permissions for databases folder.

--- create_db.sh
+++ create_db.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-mkdir site/databases 2>/dev/null
-chmod a+w -R site/databases
+mkdir -p site/databases
+chmod a+w site/databases
 
 rm site/databases/database.sqlite 2>/dev/null
 sqlite3 site/databases/database.sqlite < db/dump.sql
+chmod a+w site/databases/database.sqlite
