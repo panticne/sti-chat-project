d6e437a79773e9ca1fc7c918789b9d579f84d559
Nemanja Pantic <nemo.pantic@gmail.com>
2020-01-08 13:43:45 +0100

    Password strong

--- site/html/util/user.php
+++ site/html/util/user.php
@@ -27,6 +27,9 @@ function delete_user($userId)
 
 function update_user($user)
 {
+    if (!check_password_strong($user['password'])) {
+        return false;
+    }
     try {
         $req = 'UPDATE user SET firstname = :firstname, lastname = :lastname, password = :password, admin = :admin, active = :active WHERE id = :id';
         $val = ['firstname' => $user['firstname'],
@@ -51,6 +54,9 @@ function update_user($user)
 
 function update_password($userId, $newPassword)
 {
+    if (!check_password_strong($newPassword)) {
+        return false;
+    }
     try {
         $stmt = $GLOBALS['db']->prepare('UPDATE user SET password = :password WHERE id = :id');
         $stmt->execute(['password' => password_hash($newPassword, PASSWORD_BCRYPT), 'id' => $userId]);
@@ -67,6 +73,9 @@ function create_user($user)
         return false;
     }
 
+    if (!check_password_strong($user['password'])) {
+        return false;
+    }
     try {
         $stmt = $GLOBALS['db']->prepare('INSERT INTO user (firstname, lastname, username, password, admin, active) VALUES (:firstname, :lastname, :username, :password, :admin, :active)');
         $stmt->execute(['firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'username' => $user['username'], 'password' => password_hash($user['password'], PASSWORD_BCRYPT), 'admin' => isset($user['admin']), 'active' => isset($user['active'])]);
@@ -75,6 +84,7 @@ function create_user($user)
         echo $e->getMessage();
         return false;
     }
+
 }
 
 function get_all_users()
@@ -112,3 +122,19 @@ function get_user_with_username($username)
     }
 }
 
+function check_password_strong($password)
+{
+
+// Validate password strength
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+
+    if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 10) {
+        echo 'Password should be at least 10 characters in length and should include at least one upper case letter, one number, and one special character.';
+        return false;
+    } else {
+        return true;
+    }
+}
